09a2921af770e4daa408d3d7fa70bddc4be0b85e
SaiyanPrideKakarot <rohanbawane.89@gmail.com>
2022-09-06 23:58:33 +0530

    final update

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -1,6 +1,8 @@
 const BlogModel = require('../models/blogModel')
 const AuthorModel = require('../models/authorModel')
 
+//--------------------------------------------------------CREATE BLOG---------------------------------------------------------------
+
 const createBlog = async function (req, res) {
     try {
         let data = req.body
@@ -17,14 +19,16 @@ const createBlog = async function (req, res) {
     }
 }
 
+//--------------------------------------------------------GET BLOGS-----------------------------------------------------------------
+
 const getBlogs = async function (req, res) {
     try {
         let data = req.query
         if (Object.keys(data).length !== 0) {
-            const requireblogs = await BlogModel.find({ isDeleted: false, isPublished: true }).populate('authorId')
-            return res.status(200).send({ msg: "Require Blogs", status: true, data: requireblogs })
+            const requireblogs = await BlogModel.find({ isDeleted: false, isPublished: true }, data).populate('authorId')
+            return res.status(200).send({ msg: "Required Blogs", status: true, data: requireblogs })
         }
-        let getblogs = await BlogModel.find({ isDeleted: false, isPublished: true }, data).populate('authorId')
+        let getblogs = await BlogModel.find({ isDeleted: false, isPublished: true }).populate('authorId')
         return res.status(200).send({ msg: "All Blogs", status: true, data: getblogs })
     }
     catch (err) {
@@ -33,14 +37,16 @@ const getBlogs = async function (req, res) {
     }
 }
 
+//--------------------------------------------------------UPDATE BLOGS---------------------------------------------------------------
+
 const updateBlogs = async function (req, res) {
     try {
         let blogId = req.params.blogId
         if (!blogId) {
             return res.status(404).send({ status: false, msg: "NOT FOUND" })
         } else {
-            let isDeleted = await BlogModel.findById(blogId).select({ isDeleted: 1, _id: 0 })
-            if (isDeleted.isDeleted == true) { return res.status(404).send({ status: false, msg: "Blog not found" }) }
+            let check = await BlogModel.findById(blogId).select({ isDeleted: 1, _id: 0 })
+            if (check.isDeleted == true) { return res.status(404).send({ status: false, msg: "Blog not found" }) }
             let data = req.body
             if ((data.publishedAt)) {
                 let publishedupdate = await BlogModel.findOneAndUpdate(
@@ -67,19 +73,22 @@ const updateBlogs = async function (req, res) {
     }
 }
 
+//--------------------------------------------------------DELETE BLOGS BY ID----------------------------------------------------------
+
 const deleteBlogsById = async function (req, res) {
     try {
         let blogId = req.params.blogId
         if (!blogId) {
             return res.status(404).send({ status: false, msg: "NOT FOUND" })
         } else {
-            let isDeleted = await BlogModel.findById(blogId).select({ isDeleted: 1, _id: 0 })
-            if (isDeleted.isDeleted == true) { return res.status(404).send({ status: false, msg: "Already deleted" }) }
+            let check = await BlogModel.findById(blogId).select({ isDeleted: 1, _id: 0 })
+            if (check.isDeleted == true) { return res.status(404).send({ status: false, msg: "Already deleted" }) }
             let deleteblogs = await BlogModel.findByIdAndUpdate(
                 { _id: blogId },
                 { $set: { isDeleted: true } },
                 { new: true }
             )
+            console.log(deleteblogs)
             return res.status(200).send({ msg: "Blog deleted successful", status: true })
         }
     }
@@ -88,6 +97,8 @@ const deleteBlogsById = async function (req, res) {
         return res.status(500).send({ status: false, error: err.message })
     }
 }
+
+//--------------------------------------------------------DELETE BLOGS--------------------------------------------------------------
 
 const deleteBlogs = async function (req, res) {
     try {
@@ -99,6 +110,7 @@ const deleteBlogs = async function (req, res) {
                 { data },
                 { $set: { isDeleted: true } }
             )
+            console.log(blogsDeleted)
             return res.status(200).send({ status: true, msg: "Blogs deleted successfully" })
         }
     } catch (err) {
@@ -107,8 +119,13 @@ const deleteBlogs = async function (req, res) {
     }
 }
 
+//--------------------------------------------------------EXPORTS-------------------------------------------------------------------
+
+
 module.exports.createBlog = createBlog
 module.exports.getBlogs = getBlogs
 module.exports.updateBlogs = updateBlogs
 module.exports.deleteBlogsById = deleteBlogsById
 module.exports.deleteBlogs = deleteBlogs
+
+//-----------------------------------------------------------END-------------------------------------------------------------------
