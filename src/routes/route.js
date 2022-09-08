d9b5f42d9dcf274c102f96ad53879817e4938e5f
SaiyanPrideKakarot <rohanbawane.89@gmail.com>
2022-09-08 19:15:20 +0530

    updated

--- src/routes/route.js
+++ src/routes/route.js
@@ -6,11 +6,11 @@ const {authentication , authorisation} = require('../middlewares/middleware')
 
 
 router.post('/authors', AuthorController.createAuthor)
-router.post("/blogs", authentication , BlogController.createBlog)
-router.get('/blogs', authentication , BlogController.getBlogs)
-router.put('/blogs/:blogId',authentication , authorisation, BlogController.updateBlogs)
-router.delete('/blogs/:blogId',authentication , authorisation, BlogController.deleteBlogsById)
-router.delete('/blogs',authentication , authorisation,BlogController.deleteBlogs)
+router.post("/blogs", authentication, BlogController.createBlog)
+router.get('/blogs', authentication, BlogController.getBlogs)
+router.put('/blogs/:blogId', authentication, authorisation, BlogController.updateBlogs)
+router.delete('/blogs/:blogId', authentication, authorisation, BlogController.deleteBlogsById)
+router.delete('/blogs', authentication, authorisation ,BlogController.deleteBlogs)
 router.post('/login', AuthorController.loginUser)
 
 
